f0558dcf6ce897a15406be9d0f69afa87568eebf
henrique <henriquelc@dcc.ufrj.br>
2023-04-26 08:16:23 -0300

    redo

--- grafos.cpp
+++ grafos.cpp
@@ -1,108 +1,88 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
 
-struct no{
-    int data;
-    no *prox;
+
+template <typename T>
+class no{
+    public:
+        T data;
+        no *prox;
 };
 
-
-class lista{
-    private:
-        no *ptlista;
-    
+template<typename T>
+class nos{
     public:
+        no<T> *cbc;
         
-        lista(void){
-            ptlista = new no;
-            ptlista->prox = nullptr;
+        nos(void){
+            ptr = new no<T>;
+            cbc->prox = nullptr;
         }
 
-        void anexa(int n){
-            no* ptr = ptlista;
+        void insere(T valor){
+            no* ptr = cbc;
             while(ptr->prox != nullptr){
                 ptr = ptr->prox;
             }
 
-            no *novo = new no;
-            novo->data = n;
+            no* novo = new no;
+            novo->data = valor;
             novo->prox = nullptr;
 
             ptr->prox = novo;
         }
+};
 
-        no *primeiro(void){
-            return ptlista -> prox;
+template<typename T>
+class vetor{
+    private:
+        T *data;
+        int tamanho_alocado;
+        int tamanho_imaginario;
+        
+        void overflow(void){
+            //aloca dobro do tamanho
+            T *ptr = new T[tamanho_alocado*2];
+            for(int i = 0; i < tamanho_imaginario; i++){
+                *(ptr + i) = *(data + i);
+            }
+            
+            delete[] data;
+            data = ptr;
+        }
+
+    public:
+
+        vetor(){
+            tamanho_alocado = 10;
+            tamanho_imaginario = 0;
+            data = new T[tamanho_alocado];
+        }
+
+        ~vetor(){
+            delete[] data;
+        }
+        
+        void push_back(T coisa){
+            *(data + tamanho_imaginario) = coisa;
+            if(tamanho_imaginario > tamanho_alocado - 1){
+                overflow();
+            }
         }
 };
 
-
-std::vector<int> ordem(std::vector<lista> adjacentes){
-    std::vector<int> prioridades(adjacentes.size()); //quantos elementos apontam para i
-    /* fila de prioridades comeca tudo zerado*/
-    for(size_t i = 0; i < prioridades.size(); i++){
-        prioridades.push_back(0);
-    }
-
-    
-    /*encher a fila de prioridades*/
-    for(size_t i = 0; i < adjacentes.size(); i++){
-        no* ptr = adjacentes.at(i).primeiro();
-        while(ptr != nullptr){
-            prioridades.at(ptr->data)++;
-            ptr = ptr->prox;
-        }
-        
-    }
-
-    std::vector<int> ordem; // a ordem
-    //acha os primeiros elementos da ordem (os que  nao sao precedidos por ngm)
-    for(size_t i = 0; i < prioridades.size(); i++){
-        if(prioridades.at(i) == 0) ordem.push_back(i);
-    }
-    
-    /*agora podemos comecar o algoritmo*/
-    for(size_t j = 0; j < ordem.size(); j++){
-        
-        //vertice que usaremos
-        size_t vertice = ordem.at(j);
-
-        //para cada conexao do vertice, remover na lista de prioridades
-        no* conexao = adjacentes.at(vertice).primeiro();
-        while(conexao != nullptr){
-            size_t tarefa = conexao->data;
-            
-            prioridades.at(tarefa)--;
-            
-            if(prioridades.at(tarefa) == 0){
-                //precendentes ja foram contabilizados adicionar a lista de ordem
-                ordem.push_back(tarefa);
-            }
-
-            conexao = conexao->prox;
-        }
-    }
-
-    return ordem;
-
-}
-
 int main(void){
-	
-
-    //ler varios numeros em uma linha
-    std::vector<lista> adj;
-    std::string linha;
-    std::stringstream ss;
-    int num;
-    int vertice = 0;
-    while(std::cin >> linha){
-        ss << linha;
-        lista nova;
-        while(ss >> num){
-            nova.anexa(num);
-        }
-        ss.str("");
-        vertice++;
+    
+    char *line;
+    size_t str_size = 80;
+    line = new char[str_size];
+    while(true){
+        getline(&line,&str_size,stdin);
+        if(feof(stdin)) break;
+        printf("string : %s",line);
     }
 
+    return 0;
 }
+
+
+
