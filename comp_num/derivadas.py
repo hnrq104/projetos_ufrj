4253f92202a779bbd2276d8d5b6e59fd038a5cb7
henrique <henriquelc@dcc.ufrj.br>
2023-06-03 02:23:46 -0300

    diferencas prog dinamica

--- comp_num/derivadas.py
+++ comp_num/derivadas.py
@@ -18,7 +18,7 @@ def approx_ss_der(foo,x0,h):
     return der/h**2
 
 def approx_t_der(foo,x0,h):
-    der = 15*foo(x0 + 3*h) - 3*foo(x0 + 4*h) - 24*foo(x0 + 2*h)
+    der = 14*foo(x0 + 3*h) - 3*foo(x0 + 4*h) - 24*foo(x0 + 2*h)
     der += 18*foo(x0 + h) - 5*foo(x0)
 
     return der/2*(h**3) 
@@ -27,82 +27,87 @@ def approx_tt_der(foo,x0,h):
     der = foo(x0 + 2*h) - 2*foo(x0 + h) + 2*foo(x0 - h) - foo(x0 - 2*h)
     return der/(2*(h**3))
 
-##professor nao gostou
 
-def bar(vlist, x0, h):
-    def foobar(x):
-        return vlist[math.ceil((x - x0)/h)]
-    return foobar
+def primeira_avancada(v,h):
+    #v[] = fx, f(x + h) , f(x + 2h)
+    soma = -3*v[0] + 4*v[1] - v[2]
+    return soma/(2*h)
 
+def primeira_centrada(v,h):
+    #v  = f(x-h),f(x),f(x+h)
+    soma = v[2] - v[0]
+    return soma/(2*h)
 
+def segunda_avancada(v,h):
+    soma = 2*v[0] - 5*v[1] + 4*v[2] - v[3]
+    return soma/h**2
 
-##depois
+def segunda_centrada(v,h):
+    soma = v[0] - 2*v[1] + v[2]
+    #-h , 0, + h
+    return soma/(h**2)
 
-def primeira_dev_lista(vx, vy,j):
-    der = -vy[j + 2] + 4*vy[j + 1] - 3*vy[j]
-    return der/(vx[j + 2] - vx[j])
+def terceira_avancada(v,h):
+    soma = -5*v[0] + 18*v[1] - 24*v[2] +  14*v[3] - 3*v[4]
+    return soma/2*(h**3)
 
-def segunda_dev_lista(vx, vy,j):
-    der = -vy[j + 3] + 4*vy[j + 2] - 5*vy[j + 1] + 2*vy[j]
-    return der/((vx[j + 1] - vx[j])**2)
+def terceira_centrada(v,h):
+    #-2h, -h, h, 2h
+    soma = -v[0] + 2*v[1] - 2*v[2] + v[3]
+    return soma/2*(h**3)
+    
 
-def terceira_dev_lista(vx,vy,j):
-    der = 15*vy[j + 3] - 3*vy[j + 4] - 24*vy[j + 2]
-    der += 18*vy[j + 1] - 5*vy[j]
+# def square(x):
+#     return x**2
 
-    return der/(2*((vx[j+1] - vx[j])**3)) 
-
-
-
-def square(x):
-    return x**2
-
-def square_root(x,termos):
-    n = x
-    for i in range(termos):
-        n = (n + x/n)/2
-    return n
+# def square_root(x,termos):
+#     n = x
+#     for i in range(termos):
+#         n = (n + x/n)/2
+#     return n
 
 def sen_sq(x):
-    return sin_cos.sin_taylor(square_root(x,10),10)/square_root(x,10)
+    sqr = math.sqrt(x)
+    return math.sin(sqr)/sqr
 
 
-def diff(h):
-    vx = []
-    vy = []
-    i = 0
-    while(i < 1):
-        i+=h
-        vx.append(i)
-        vy.append(sen_sq(i))
+def derivada(x):
+    sqr = math.sqrt(x)
+    return (math.cos(sqr)*sqr - math.sin(sqr))/(2*x*sqr)
 
-    print("H = ",h,"\n")
-    print("Avaliação da primeira derivada em : 1/4")
-    print("primeira por lista", primeira_dev_lista(vx,vy,math.floor((1/h)*(1/4))))
-    print("primeira por funcao", approx_p_der(sen_sq,0.25,h))
-    print("diff = ",primeira_dev_lista(vx,vy,math.floor((1/h)*(1/4))) - approx_p_der(sen_sq,0.25,h))
+def diff(h,x):
+    #algebrica
+    resp = []
+    resp.append(derivada(x))
 
-    print("---------------------------------------------------------\n")
+    #avancada
+    lista1 = []
+    for i in range(3):
+        lista1.append(sen_sq(x + i*h))
+    
+    resp.append(primeira_avancada(lista1,h))
 
-    print("Avaliação da primeira derivada em : 1/2")
-    print("primeira por lista", primeira_dev_lista(vx,vy,math.floor((1/h)*(1/2))))
-    print("primeira por funcao", approx_p_der(sen_sq,0.5,h))
-    print("diff = ",primeira_dev_lista(vx,vy,math.floor((1/h)*(1/2))) - approx_p_der(sen_sq,0.5,h))
-
-    print("---------------------------------------------------------\n")
-
-    print("Avaliação da primeira derivada em : 3/4")
-    print("primeira por lista", primeira_dev_lista(vx,vy,math.floor((1/h)*(3/4))))
-    print("primeira por funcao", approx_p_der(sen_sq,0.75,h))
-    print("diff = ",primeira_dev_lista(vx,vy,math.floor((1/h)*(3/4))) - approx_p_der(sen_sq,0.75,h))
-
-    print("\n")
-    return
+    #centrada
+    lista2 = []
+    for i in range(-1,2):
+        lista2.append(sen_sq(x + i*h))
+    
+    resp.append(primeira_centrada(lista2,h))
+    
+    return resp
 
 
-vx = [0,0.002,0.006,0.012,0.018,0.024]
-vy = [0,0.287,0.899,1.915,3.048,4.299]
+h_values = [0.1,0.01,0.001,0.0001]
+x_values = [0.25, 0.5, 1]
 
-u = 1.8 * (10**(-5))
-print(u * primeira_dev_lista(vx,vy,0))
+for i in x_values:
+    print("funcao avaliada em : ", i)
+    for j in h_values:
+        resp = diff(j,i)
+        print("h = ", j)
 
+        print("algebrica = ", resp[0])
+        print("avancada = ", resp[1], " diff = ", resp[1] - resp[0])
+        print("centrada = ", resp[2], " diff = ", resp[2] - resp[0])
+
+        print("\n")
---
+++ comp_num/diferenca.py
@@ -0,0 +1,55 @@
+import math
+
+#diferencas divididas de newton
+def diferenca(dif_0, dif_1, x_0, x_1):
+    return (dif_0 - dif_1)/(x_0 - x_1)
+
+def n2_bottom_up(f_list : list, x_list : list):
+    matrix = []
+    matrix.append(f_list) #order 0
+    #will do n^2 first, then O(n) for memory
+    for i in range(1,len(f_list)):
+        linha = []
+        for j in range(len(f_list) - i):
+            #unneeded but for clarity sake
+            dif_0 = matrix[i-1][j]
+            dif_1 = matrix[i-1][j+1]
+            x_0 = x_list[j]
+            x_1 = x_list[j+i]
+            linha.append(diferenca(dif_0,dif_1,x_0,x_1))
+        matrix.append(linha)
+
+    importantes = []
+    for arr in matrix:
+        importantes.append(arr[0])
+    
+    return importantes
+
+def n_bottom_up(f_list : list, x_list : list):
+    vector = f_list.copy()
+    for i in range(len(vector)):
+        pos = len(vector) - i - 1
+        for j in range(i):
+            dif_0 = vector[pos + j]
+            dif_1 = vector[pos + j + 1]
+            x_0 = x_list[pos]
+            x_1 = x_list[pos + j + 1]
+
+            vector[pos + 1 + j] = diferenca(dif_0,dif_1,x_0,x_1)
+
+    return vector
+            
+    return 0
+         
+
+valoresx = [0, 0.35, 0.65, 1]
+tabelae = [1, 1.4190675, 1.9155408, 2.7182818]
+
+resp = n2_bottom_up(tabelae, valoresx)
+for i in resp:
+    print(i)
+
+print("tentativa o(n)")
+resp = n_bottom_up(tabelae,valoresx)
+for i in resp:
+    print(i)
