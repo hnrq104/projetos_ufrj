7017f497f7283de4c8961bbc9067113d7cebe1ec
henrique <henriquelc@dcc.ufrj.br>
2023-06-13 23:39:33 -0300

    deus me abandonou, mas consegui

--- estrutura_dados/heap_binomial.cpp
+++ estrutura_dados/heap_binomial.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 struct fruta{
     float chave;
     int ordem;
@@ -23,6 +22,7 @@ struct floresta{
         saladas = nullptr; /*head*/
     }
 
+    /*lembrar que preservamos a preservamos a próxima de b*/
     fruta* mistura_salada(fruta* a, fruta* b){
         fruta* menor;
         fruta* subjugada;
@@ -72,40 +72,122 @@ struct floresta{
         return inserida;
     }
 
+    fruta* diminui_chave(fruta* ptr, float n){
+        if(ptr->chave > n){
+            /*operacao invalida*/
+            return ptr;
+        }
+
+        ptr->chave = n;
+
+        while(ptr->ant != nullptr && ptr->ant->chave > n){
+            ptr->chave = ptr->ant->chave;
+            ptr->ant->chave = n;
+            ptr = ptr->ant;
+        }
+    }
+
+    float extrai_minimo(void){
+        if(saladas == nullptr){
+            throw std::runtime_error("nada na heap");
+        }
+
+        fruta* extraido = min;
+
+        float r = extraido->chave;
+        fruta* sem_galho = extraido->sub;
+        fruta* desconectado = extraido->prox;
+        delete extraido;
+
+
+        
+        std::vector<fruta*> apatridas; //esse vetor tá ao contrario
+        fruta* minima = sem_galho;
+        
+        while(sem_galho != nullptr){
+            if(sem_galho->chave < minima->chave) minima = sem_galho;
+
+            fruta* tmp = sem_galho;
+            apatridas.push_back(sem_galho);
+            sem_galho = sem_galho->prox;
+            tmp->ant = nullptr;
+            tmp->prox = nullptr;
+        }
+
+        int tam = apatridas.size();
+        std::vector<fruta*> arvores(tam + 1);
+        fruta* ptr = saladas;
+        for(int ordem = 0; ordem < tam + 1; ordem++){
+            if(ptr->ordem == ordem){
+                
+                if(ptr->chave < minima->chave) minima = ptr;
+                
+                arvores.at(ordem) = ptr;
+                ptr = ptr->prox;
+            }
+            else{
+                arvores.at(ordem) = nullptr;
+            }
+        }
+        
+        for(int ordem = 0; ordem < tam; ordem++){
+            fruta* a = apatridas.at(tam - ordem - 1);
+            
+            int i = 0;
+            while(arvores.at(ordem + i) != nullptr){
+                a = mistura_salada(a,arvores.at(ordem + i));
+                arvores.at(ordem + i) = nullptr;
+                i++;
+            }
+
+            arvores.at(ordem + i) = a;
+        }
+
+        fruta* prato = nullptr;
+        size_t i = 0;
+        while(arvores.at(i) == nullptr) i++;
+        prato = arvores.at(i);
+        saladas = prato;
+
+        for(i = i+1; i < arvores.size(); i++){
+            if(arvores.at(i) != nullptr){
+                prato->prox = arvores.at(i);
+                prato = prato->prox;
+            }
+        }
+
+        prato->prox = desconectado;
+        
+        prato = desconectado;
+        while(prato != nullptr){
+            if(prato->chave < minima->chave){
+                minima = prato;
+            }
+        }
+
+        min = minima;
+        return r;
+    }
 };
 
 
-void printa_fruta(fruta* ptr){
-    std:: cout << "end = " << ptr;
-    std::cout << " chave = " << ptr->chave;
-    std::cout << " ordem = " << ptr->ordem;
-    std::cout << " pai = " << ptr->ant;
-    std:: cout << " filho = " << ptr->sub;
-}
 int main(void){
     floresta h;
-    fruta* pt[5];
-    pt[4] = h.insere(5.0);
-    pt[3] = h.insere(4.0);
-    pt[2] = h.insere(3.0);
-    pt[0] = h.insere(1.0);
-    pt[1] = h.insere(2.0);
+    h.insere(10.0);
+    h.insere(20.0);
+    h.insere(7.0);
+    h.insere(13.0);
 
-    for(int i = 0; i < 5; i++){
-        printa_fruta(pt[i]);
-        std::cout << std::endl;
-    }
+    std::cout << "extrai h :"  << h.extrai_minimo() << std::endl;
+    h.insere(30);
+    std::cout << "extrai h :"  << h.extrai_minimo() << std::endl;
     
+    h.insere(25);
+    std::cout << "extrai h :"  << h.extrai_minimo() << std::endl;
+    std::cout << "extrai h :"  << h.extrai_minimo() << std::endl;
 
-    // fruta* ptr = h.saladas;
-    // std::cout << h.min->chave << std::endl; 
+    std::cout << "min h = " << h.min->chave << std::endl;
 
-    // while(ptr!= nullptr){
-    //     std::cout << "chave :" << ptr->chave;
-    //     std::cout << " ordem :" << ptr->ordem  << std::endl;
-
-    //     ptr = ptr->prox;
-    // }
 
     return 0;
 }
